db0bc3d2fd1d2c76dd234864cfce95ec7afab7ca
Rohankhan999 <159823468+Rohankhan999@users.noreply.github.com>
2024-10-05 23:20:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ for (var i = 0; i < Numbers.length; i++) {
     }
 }
 document.write("<H1>" + "SORTED ARRAY" +"<br>" + Numbers + "</h1>")
+
+// question2
+var numbers = [1, 2, 3, 5]; 
+
+
+var n = numbers.length + 1; 
+var expectSum = (n * (n + 1)) / 2;
+
+
+var actualSum = 0;
+for (var i = 0; i < numbers.length; i++) {
+    actualSum += numbers[i];
+}
+
+
+var missingNumber = expectSum - actualSum;
+
+console.log(missingNumber);
